b0ca69e2257aee90dad2ac9c67478826b6e433d3
oskargrimminger <oskargrimminger@gmail.com>
2024-09-23 14:52:07 +0530

    add

--- stockprice.py
+++ stockprice.py
@@ -1,7 +1,5 @@
-
 import csv
 from datetime import datetime
-
 
 monthly_data = {
     'January': [],
@@ -21,9 +19,14 @@ monthly_data = {
 with open('stockdata.csv', 'r') as file:
     csv_reader = csv.DictReader(file)
 
-    for row in csv_reader:   	
-    	date = datetime.strptime(row['Date'], '%Y-%m-%d')
-    	if date.year == 2007:
+    
+    for row in csv_reader:
+        try:
+            date = datetime.strptime(row['Date'], '%Y-%m-%d')
+        except Exception:
+            pass
+
+        if date.year == 2007:
             data = {
                 'MSFT': float(row['MSFT']),
                 'IBM': float(row['IBM']),
@@ -57,15 +60,17 @@ with open('stockdata.csv', 'r') as file:
                 monthly_data['November'].append(data)
             elif date.month == 12:
                 monthly_data['December'].append(data)
+    
+    
 
 def calculate_candlestick(data, company):
     if len(data) == 0:
         return None
     
-    open_price = data[0][company]
-    close_price = data[-1][company]
-    high_price = max([entry[company] for entry in data])
-    low_price = min([entry[company] for entry in data])
+    open_price = data[0][company]  
+    close_price = data[-1][company]  
+    high_price = max([entry[company] for entry in data])  
+    low_price = min([entry[company] for entry in data])  
     
     return {
         'Open': open_price,
@@ -74,6 +79,7 @@ def calculate_candlestick(data, company):
         'Low': low_price
     }
 
+
 for month, data in monthly_data.items():
     print(f"Candlestick data for {month}:")
     for company in ['MSFT', 'IBM', 'SBUX', 'AAPL', 'GSPC']:
@@ -81,4 +87,5 @@ for month, data in monthly_data.items():
         if candlestick:
             print(f"{company}: {candlestick}")
     print("\n")
+
 
